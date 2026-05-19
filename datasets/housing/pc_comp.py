fee842455852476f9245dd42a34eb171a24a64e8
Neima <a01399122@gmail.com>
2026-05-18 22:40:22 -0300

    kernel ipca

---
+++ datasets/housing/pc_comp.py
@@ -0,0 +1,6 @@
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("sudhanshuy17/pccomponents")
+
+print("Path to dataset files:",'C:\\Users\\augusto\\.vscode\\Data Sci and Ml\\Linear_Regression\\datasets')
